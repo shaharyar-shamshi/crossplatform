322a67d330a86165a294b6b6a1b574e72923945f
shaharyar-shamshi <shaharyarshamshi@gmail.com>
2017-07-04 19:17:04 +0530

    this contain the signup page ,login page other accessories

---
+++ WebContent/signupsuccess.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<p> <h3> You are successfully registered</h3></p>
+<p> <a href="login.html"/> <button type="button">login </button></p>
+</body>
+</html>
---
+++ src/signup/usercheck.java
@@ -0,0 +1,86 @@
+package signup;
+import java.sql.*;
+
+public class usercheck {
+	public static void main(String[] args)
+	{
+		usercheck u=new usercheck();
+		u.connect("sha", "samik", "password", "merchant", "gender", "emailid");
+	}
+   public void connect(String name,String userid,String password,String merchant,String gender,String emailid)
+   {
+	   int k=0;
+	   Connection conn=null;
+       String id="SHAHARYARSHAMSHI";
+       String passcode="shahbazshamshi";
+       String url="jdbc:mysql://localhost/cookbook?useSSL=false";
+       try
+       {
+          Class.forName("com.mysql.jdbc.Driver").newInstance();
+          conn=DriverManager.getConnection(url, id, passcode);
+          System.out.println("connected");
+       }
+       catch (Exception e)
+       {
+           System.out.println(e);
+       }
+       try
+       {
+    	   PreparedStatement s;
+    	   s=conn.prepareStatement("select * from user where username =?");
+    	   s.setString(1, userid);
+    	   ResultSet rs=s.executeQuery();
+    	 if(!rs.next())
+    	 {
+    		k=0;
+    	 }
+    	 else
+    	 {
+    		 k++;
+    	 }
+    	   System.out.println(k);
+    	   s.close();
+    	   rs.close();
+       }
+       catch(Exception e)
+       {
+    	   System.out.println(e);
+       }
+       if(k==0)
+       {
+    	   
+    	   try {
+    		   PreparedStatement p;
+			p=conn.prepareStatement("INSERT INTO user(name,username,password,merchant,gender,emailid,mobile,balance)values(?,?,?,?,?,?,?,?)");
+			p.setString(1, name);
+			p.setString(2, userid);
+			p.setString(3,password);
+			p.setString(4, merchant);
+			p.setString(5, gender);
+			p.setString(6,emailid);
+			p.setInt(7, 988);
+			p.setInt(8,0);
+			p.executeUpdate();
+			
+			p.close();
+		} 
+    	   catch (SQLException e)
+    	   {
+			System.out.println("error");
+		}
+       }
+       if(conn!=null)
+       {
+    	   try
+    	   {
+    		   conn.close();
+    	   }
+    	   catch(Exception e)
+    	   {
+    		   System.out.println();
+    	   }
+       }
+       //return k;
+       
+   }
+}
